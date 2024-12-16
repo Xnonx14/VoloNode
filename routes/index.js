f6fe5ebc9715caf92545e00981d9d00eee5db2a6
Xnonx14 <kchang096@gmail.com>
2024-12-16 14:20:17 -0500

    .env for volo token

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', async function(req, res, next) {
   var temp2 = await volo.checkGameRegistration("675075ce0195f60cceccab79")
   var temp3 = await volo.checkRoster('675075ce0195f60cceccab79')
   
-  // console.log(temp1)
+  console.log(temp1)
   console.log(temp2)
   console.log(temp3)
   res.render('index', { title: 'Express' });
--- utils/volo-utils.js
+++ utils/volo-utils.js
@@ -17,7 +17,12 @@ class Volo{
             data: data,
             headers: headers
         })
-        return response.data['data']['discoverDaily']['programs']
+        var result = []
+        var programs = response.data['data']['discoverDaily']['programs']
+        programs.forEach(e=>{
+            result.push(e.leagueId)
+        })
+        return result
     }
 
     async checkGameRegistration(league_id){
@@ -26,7 +31,7 @@ class Volo{
         //var headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
         //var cookies = {'volosports_token':'Bearer%20eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NjVlOGM5NTY1N2NkMzk3OWMxZTk2ZjEiLCJpYXQiOjE3MzA3ODUyODAsImV4cCI6MTc2MjMyMTI4MH0.elGW6TIkfMVftty14QEV3DwuTrJwp6BlWC1QBaj6YVhPFDBo0olxTapS6H4ZNsgVn_0y-XMl3v32wwTJWZAlcisxt3CyPDPxyaE7tBwwaooMMYh9EcMK-3hm-vlisPQ_39eYvWySV9SRF9SfSiLdRRizC_mcprYeUFZYBI_EqNjLoe3nM7TPHfH_gd0cx-7vxmGwKalOD3q-4QtHRfxK_WKAjwDJY75J9Vom3mbqPBbheeuVM9_2a-pfKP9Cb0rzoFk13BV4EvC92x5TUGo-uTlCg_i6GaDbOIMarWRzmAT2phSQrWaOmJFZaXNAcuo7eaJLuBwG3nNP9t-felCGfQJxGUW6P81qXBTo6QjUyhdF43cchEriYN_AJu3k1wfRVPMZTwRN2chwC56JpGKokFkqL5HCCTMd0j-t4eDQNz8T1fvDSMYe6lziOTijv7AwX5HFp2gfW2TgrHbBx8HIpnzcSuIeJXy_-FzMy2Amx4zBRFjwdtnTNZm5VJDoFnDTBlWpfC95aR88Lk6xB5AgRn17brCfu4S30YPil8mzdL-nW0CHyqZ7FqP3NpGrwyahIrSDJ7op7KmpE1BbW3ZS2ZrWn0fgLseVw2TLapkyOCd-BR48SizknE4USjW25UFSliq6n630QIAUdlydGNPx-kAn33QxqMzmqlkdYMQ9Y8c'}
         payload = payload.replace("replace_id_here",league_id)
-        var cookies = 'volosports_token=Bearer%20eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NjVlOGM5NTY1N2NkMzk3OWMxZTk2ZjEiLCJpYXQiOjE3Mjc5NzkzMjAsImV4cCI6MTc1OTUxNTMyMH0.QwxHySJ4fvkg8UkNyd_PQyWKVuufcwkogVYhXoghx5qRR81pCh_Z7YNE_qhiwSCXoloZQnrX_HsCDd8dZTuPQNIj3gou797j6UaSvGS3dZdAmD8TG1Kyj3KZN31i08OBJgBPN3K4N8D2JHa15fT2BqdZhP40qLQTA4zcIZ8zW6NPDWx4iMiexaejvZZLMWVBZkmnPBx2kSixK3YNeorew6YCnvI0XZwwSWc6vSx7-G0O1IfBIucYdoNWtb3PCkwe1d-g7aSGMokIQxP5abaAtq8f2wkIaamQebB9eezd2i8hyDDWy__y-PWkefDJJppndeIe9RTSNSBl--ey-Xu_cTJLCbWiQwgJSL4Jyi9--NtJrZeQNolYDdKtYMTRSsaobr_sbA5Td10KPBj_YI2TuGJ9kaigAeFXQqAHgztP2IhwJw6LdSUK6Ig-6bDLm628JWjPCTdLDZgLwXRNp-RcWLY7uR6qOc5gRYKmf3baaZ_CdsJ2kIQbvnYmhaeUViZbWwc8H-JVYwlJFHH__O1BJXtvmJ1GU5XPHT9bjlJAbOzI7QHa-WACoFiHOPfsWdlwSUVhZi0o4aYThfvOHrSNAj2FrOKDUpIX_lAHZZLsleG3jhGeNzYdU0dy9WWSX3kQ1ARW7mMSIyscMNj9ZgNgNcyAJnKJObuk5zbGF-i_wPE;'
+        var cookies = process.env.VOLO_TOKEN
         var headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8', 'Cookie':cookies}
         // r = requests.post(url, data=payload, headers=headers,cookies=cookies)
         var response = await axios({
@@ -42,7 +47,7 @@ class Volo{
         var url = 'https://www.volosports.com/graphql'
         var payload = '{"operationName":"programRoster","variables":{"leagueId":"replace_id_here"},"query":"query programRoster($leagueId: ID\u0021) {\\n  programRoster(leagueId: $leagueId) {\\n    userId\\n    gender\\n    picture\\n    userDisplayInfo {\\n      displayName\\n      displayEmail\\n      displayPhone\\n      __typename\\n    }\\n    __typename\\n  }\\n}"}'
         payload = payload.replace("replace_id_here",league_id)
-        var cookies = 'volosports_token=Bearer%20eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NjVlOGM5NTY1N2NkMzk3OWMxZTk2ZjEiLCJpYXQiOjE3Mjc5NzkzMjAsImV4cCI6MTc1OTUxNTMyMH0.QwxHySJ4fvkg8UkNyd_PQyWKVuufcwkogVYhXoghx5qRR81pCh_Z7YNE_qhiwSCXoloZQnrX_HsCDd8dZTuPQNIj3gou797j6UaSvGS3dZdAmD8TG1Kyj3KZN31i08OBJgBPN3K4N8D2JHa15fT2BqdZhP40qLQTA4zcIZ8zW6NPDWx4iMiexaejvZZLMWVBZkmnPBx2kSixK3YNeorew6YCnvI0XZwwSWc6vSx7-G0O1IfBIucYdoNWtb3PCkwe1d-g7aSGMokIQxP5abaAtq8f2wkIaamQebB9eezd2i8hyDDWy__y-PWkefDJJppndeIe9RTSNSBl--ey-Xu_cTJLCbWiQwgJSL4Jyi9--NtJrZeQNolYDdKtYMTRSsaobr_sbA5Td10KPBj_YI2TuGJ9kaigAeFXQqAHgztP2IhwJw6LdSUK6Ig-6bDLm628JWjPCTdLDZgLwXRNp-RcWLY7uR6qOc5gRYKmf3baaZ_CdsJ2kIQbvnYmhaeUViZbWwc8H-JVYwlJFHH__O1BJXtvmJ1GU5XPHT9bjlJAbOzI7QHa-WACoFiHOPfsWdlwSUVhZi0o4aYThfvOHrSNAj2FrOKDUpIX_lAHZZLsleG3jhGeNzYdU0dy9WWSX3kQ1ARW7mMSIyscMNj9ZgNgNcyAJnKJObuk5zbGF-i_wPE;'
+        var cookies = process.env.VOLO_TOKEN
         var headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8', 'Cookie':cookies}
                                            	
         var response = await axios({
