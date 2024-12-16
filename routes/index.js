bd8bf7d0ed1c5596c33e4e6585e5b95424b4acf0
Xnonx14 <kchang096@gmail.com>
2024-12-16 14:07:16 -0500

    volo node utils

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+const Volo = require('../utils/volo-utils.js');
+
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  let volo = new Volo()
+  var temp1 = await volo.getListing()
+  var temp2 = await volo.checkGameRegistration("675075ce0195f60cceccab79")
+  var temp3 = await volo.checkRoster('675075ce0195f60cceccab79')
+  
+  // console.log(temp1)
+  console.log(temp2)
+  console.log(temp3)
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+
